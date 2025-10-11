365f944b717a97898ed3aa7b0c08c392356476b3
Marcelo Augusto <marcelo.augusto.453@outlook.com>
2025-10-11 14:51:05 -0300

    feat: adding logger with loguru in schema validation

--- src/schema.py
+++ src/schema.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import pandera as pa
+from loguru import logger
 from pandera import Column, Check, DataFrameSchema
 
 AREAS_VALIDAS = ["Vendas", "TI", "Financeiro", "RH", "Operações"]
 
+
+ERRO_MAP_PANDERA = {
+    "not_nullable": "Campos nulos.",
+    "is_in": "Campos nulos",
+    
+    # Erros de Regras
+    "str_matches('^[A-Za-zÀ-ÿ\s]+$')": "Nome contém números ou caracteres inválidos.",
+    "isin(['Vendas', 'TI', 'Financeiro', 'RH', 'Operações'])": "Área inválida. Deve ser uma das permitidas.",
+    "greater_than_or_equal_to(0)": "Salário ou ID deve ser um número positivo ou zero.",
+    "between(0, 1, inclusive='both')": "Bônus percentual deve estar entre 0 e 1 (inclusive).",
+}
 
 funcionarios_schema = DataFrameSchema(
     {
@@ -21,16 +33,72 @@ funcionarios_schema = DataFrameSchema(
 )
 
 def validar_e_separar(df: pd.DataFrame, path_erros: str = "output/erros.csv"):
+    ''' 
+    Função responsável por validar o schema do pandas usando pandera e separar os arquivos entre válidos e não válidos.
+
+    ArgS: Dataframe do Pandas
+
+    Saída: arquivo de output com erros.
+
+    '''
+
+    logger.info("Iniciando transformação: validação de Schema (Pandera).")
+    
     try:
         df_validado = funcionarios_schema.validate(df, lazy=True)
-        return df_validado, pd.DataFrame(columns=df.columns)  
+        
+        logger.success(f"Validação concluída: todos os {len(df_validado)} registros são válidos.")
+        
+        return df_validado, pd.DataFrame(columns=df.columns.tolist() + ['motivo_erro']) 
 
     except pa.errors.SchemaErrors as err:
         idx_invalidos = set(err.failure_cases["index"].tolist())
-
         df_invalidos = df.loc[df.index.isin(idx_invalidos)].copy()
         df_validos = df.loc[~df.index.isin(idx_invalidos)].copy()
+
+
+        erros_agrupados = (
+            err.failure_cases
+            ['check'].map(ERRO_MAP_PANDERA).fillna('Erro de Validação Desconhecido') 
+            .groupby(err.failure_cases['index'])
+            .apply(lambda x: '; '.join(x.astype(str).unique()))
+            .rename('motivo_erro')
+        )
+
+        df_invalidos = df_invalidos.merge(
+            erros_agrupados, 
+            left_index=True, 
+            right_index=True, 
+            how='left'
+        )
+
+        df_invalidos['motivo_erro'] = df_invalidos['motivo_erro'].fillna('Erro de Validação Desconhecido')
+
+        erros_contados = df_invalidos['motivo_erro'].value_counts().head(3).to_dict()
+        
+        logger.info(f"Validação concluída: {len(df_validos)} válidos | {len(df_invalidos)} inválidos.")
+        
+        logger.info(f"Top 3 motivos de erro: {erros_contados}")
 
         df_invalidos.to_csv(path_erros, index=False)
 
         return df_validos, df_invalidos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
